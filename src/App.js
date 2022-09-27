ac9a5ae25a4d2a1720c1f7b5099d0558002183ba
acramer20 <97561089+acramer20@users.noreply.github.com>
2022-09-27 12:34:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import JkList from "./JkList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JkList />
+      </div>
+    );
+  }
+}
+
+export default App;
